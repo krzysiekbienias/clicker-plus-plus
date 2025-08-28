a39e8822ee2ff9bac8302066a987ed6fe1169fa0
Krzysiek Bienias <krzysiek.bienias.kr@gmail.com>
2025-08-28 22:09:42 +0200

    feat:add new memeber class parent

--- header/binary_tree/binary_tree_node.hpp
+++ header/binary_tree/binary_tree_node.hpp
@@ -5,12 +5,14 @@ class BinaryTreeNode {
         int m_value;
         BinaryTreeNode* left;
         BinaryTreeNode* right;
+        BinaryTreeNode* parent;
         //constructor
 
         explicit BinaryTreeNode(int value) {
             this->m_value = value;
             left = nullptr;
             right = nullptr;
+            parent= nullptr;
         }
 
         //destructor
