2989e5fb7c3dfc093127b3cdbc0a6727a84e070a
krzysiekbienias <krzysiek.bienias.kr@gmail.com>
2025-09-17 23:56:32 +0200

    feat: add solution for majority element problem  using BoyerMoore algorithm.

--- header/arrays/majority_element.hpp
+++ header/arrays/majority_element.hpp
@@ -3,3 +3,5 @@
 // Declare your majority_element interface here.
 
 int majorityElement(std::vector<int>& nums);
+int majorityElementBoyerMoore(const std::vector<int>& nums);
+int majorityElementBoyerMooreGeneralSolution(const std::vector<int>& nums);
--- src/arrays/majority_element.cpp
+++ src/arrays/majority_element.cpp
@@ -16,3 +16,42 @@ int majorityElement(std::vector<int>& nums){
     }
     return -1;
 }
+
+//unique majority element granted
+int majorityElementBoyerMoore(const std::vector<int>& nums) {
+    int candidate=-1;
+    int count=0;
+    for (int num:nums) {
+        if (count==0) {
+            candidate=num;
+            count=1;
+        }
+        else if (num==candidate) {
+            count++;
+        }
+        else {
+            count--;
+        }
+    }
+
+    return candidate;
+}
+
+
+
+int majorityElementBoyerMooreGeneralSolution(const std::vector<int>& nums) {
+    int candidate=0;
+    int count =0;
+    for (int num:nums) {
+        if (count==0) {
+            candidate=num;
+            count=1;
+        }
+        else if (num==candidate) ++count;
+        else --count;
+    }
+    int occur=0;
+    for (int num:nums) if (num==candidate) ++occur;
+
+    return (occur>static_cast<int>(nums.size()/2))? candidate:-1;
+}
