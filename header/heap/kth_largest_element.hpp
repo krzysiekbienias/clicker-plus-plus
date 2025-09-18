fd7d0a47f4c6482c653019536a76a57851ea403b
krzysiekbienias <krzysiek.bienias.kr@gmail.com>
2025-09-18 16:08:53 +0200

    feat: add function to find k greatest value as a vector of k values.

--- header/heap/kth_largest_element.hpp
+++ header/heap/kth_largest_element.hpp
@@ -3,3 +3,4 @@
 // Declare your kth_largest_element interface here.
 
 int kthGratestValue(std::vector<int> nums,int k);
+std::vector<int> kGratestValues(std::vector<int> nums,int k);
--- src/heap/kth_largest_element.cpp
+++ src/heap/kth_largest_element.cpp
@@ -17,3 +17,26 @@ int kthGratestValue(std::vector<int> nums,int k){
     }
     return minHeap.top();
 }
+
+std::vector<int> kGratestValues(std::vector<int> nums,int k) {
+    if (nums.empty() || k<=0) return {};
+    std::priority_queue<int,std::vector<int>,std::greater<int>> minHeap;
+    //we push first k element into the heap.
+    for (int i=0;i<k;++i){
+        minHeap.push(nums[i]);
+    }
+    for (int j=k;j<nums.size();++j){
+        if(nums[j]>minHeap.top()){
+            minHeap.pop();
+            minHeap.push(nums[j]);
+        }
+    }
+    std::vector<int> largestElements;
+    largestElements.reserve(k);
+    while (!minHeap.empty()) {
+        largestElements.push_back(minHeap.top());
+        minHeap.pop();
+    }
+    std::sort(largestElements.rbegin(),largestElements.rend());
+    return largestElements;
+}
