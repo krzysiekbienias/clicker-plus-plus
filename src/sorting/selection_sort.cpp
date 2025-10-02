735f35e8003cdd8fd5fa3cc248e4fd94a06774fe
krzysiekbienias <krzysiek.bienias.kr@gmail.com>
2025-10-03 00:14:24 +0200

    feat: implement selection sort algorithm

--- src/sorting/selection_sort.cpp
+++ src/sorting/selection_sort.cpp
@@ -1,6 +1,24 @@
 #include<vector>
+#include <iostream>
 
 // Implement your selection_sort logic here.
 std::vector<int> selectionSorting(std::vector<int>& array){
-    
+    // 1. empty arr
+    if (array.empty()) return {};
+    int startIdx=0;
+    //until second to last
+    while (startIdx<array.size()-1) {
+        int smallestIdx=startIdx;
+
+        for (int i=smallestIdx+1;i<array.size();++i) {
+            //block dedicated to find smallest index per suffix array
+            if (array[smallestIdx]>array[i]) {
+                smallestIdx=i;
+            }
+
+        }
+        std::swap(array[smallestIdx],array[startIdx]);
+        startIdx++;
+    }
+    return array;
 }
