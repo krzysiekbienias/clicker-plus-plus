19963963d7e902cf4fd2e18601088c488c0f6f28
Krzysiek Bienias <krzysiek.bienias.kr@gmail.com>
2025-10-06 10:54:19 +0200

    fix: Make a fix to a longest subarray problem to update indexes only if we come accross longer substring not only when we have a sum equal the target.

--- src/arrays/longest_subarray_with_sum.cpp
+++ src/arrays/longest_subarray_with_sum.cpp
@@ -31,22 +31,27 @@ std::vector<int> longestSubarrayWithSumInterval(const std::vector<int>& array, i
     int maxWindow=0;
     int bestL=-1;
     int bestR=-1;
-    //right pointer always will be moved
+    //right pointer will always be moved typically for sliding window pattern.
     for (int r=0;r<n;++r) {
-        currentSum+=array[r];
-        while (l<=r && currentSum>targetSum) {
-            currentSum-=array[l];
+        currentSum += array[r];
+        while (l <= r && currentSum > targetSum) {
+            currentSum -= array[l];
             ++l;
         }
-        if (currentSum==targetSum) {
-            candidateWindow=r-l+1;
-            maxWindow=std::max(candidateWindow,maxWindow);
-            bestL=l;
-            bestR=r;
+        if (currentSum == targetSum) {
+            candidateWindow = r - l + 1;
+            // This condition ensures we don’t overwrite a longer valid interval with a shorter one.
+            // Example: target = 7 can be formed as 1+1+1+1+1+1+1 or 3+4 — we prefer the longer interval.
+            if (candidateWindow > maxWindow) {
+                maxWindow = candidateWindow;
+                bestL = l;
+                bestR = r;
+            }
         }
     }
     if (bestL==-1) {
         return {};
     }
+
     return {bestL,bestR};
 }
