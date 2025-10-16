b995aaf044a8a7610b60fbac4d00a361783e4c8a
krzysiekbienias <krzysiek.bienias.kr@gmail.com>
2025-10-16 13:59:10 +0200

    fead: add solution for remove duplicates from linked list with leak memory prevention

--- header/linked_list/remove_duplicates.hpp
+++ header/linked_list/remove_duplicates.hpp
@@ -1,3 +1,6 @@
 #pragma once
-
+#include "linked_list/coding_platform_setup.hpp"
 // Declare your remove_duplicates interface here.
+
+
+LinkedListCodingPlatform* removeDuplicatesFromLinkedList(LinkedListCodingPlatform* linkedList);
--- src/linked_list/remove_duplicates.cpp
+++ src/linked_list/remove_duplicates.cpp
@@ -1,3 +1,20 @@
+#include "linked_list/coding_platform_setup.hpp"
+
 
 
 // Implement your remove_duplicates logic here.
+
+LinkedListCodingPlatform* removeDuplicatesFromLinkedList(LinkedListCodingPlatform* head) {
+    LinkedListCodingPlatform* current=head;
+    while (current!=nullptr && current->next!=nullptr) {
+        if (current->value==current->next->value) {
+            LinkedListCodingPlatform* duplicate=current->next;
+            current->next=current->next->next;
+            delete duplicate;
+        }
+        else
+            current=current->next;
+    }
+    return head;
+
+}
