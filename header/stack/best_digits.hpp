c51db9d478eaff3eee7d808b0e8c7dc45980c92f
krzysiekbienias <krzysiek.bienias.kr@gmail.com>
2025-10-23 23:14:22 +0200

    feat: implement solution for best digits using stack.

--- header/stack/best_digits.hpp
+++ header/stack/best_digits.hpp
@@ -1,3 +1,6 @@
 #pragma once
+#include <std_aliases.hpp>
 
+using namespace stl;
 // Declare your best_digits interface here.
+string bestDigits(string number, int numDigits);
--- src/stack/best_digits.cpp
+++ src/stack/best_digits.cpp
@@ -1,3 +1,30 @@
+#include <std_aliases.hpp>
+
+using namespace stl;
+// Declare your best_digits interface here.
+string bestDigits(string number, int numDigits) {
+    vector<int> st;
+    string result;
+    for (char digit:number) {
+        if (std::isdigit(static_cast<unsigned char>(digit))) {
+            int intDigit=digit-'0';
+            while (!st.empty() && intDigit>st.back() && numDigits>0) {
+                st.pop_back();
+                numDigits--;
+            }
+            st.push_back(intDigit);
+        }
+
+    }
+    while (numDigits>0) {
+        st.pop_back();
+        numDigits--;
+    }
+    for (int x :st) {
+        result+=std::to_string(x);
+    }
+    return result;
+
+}
 
 
-// Implement your best_digits logic here.
