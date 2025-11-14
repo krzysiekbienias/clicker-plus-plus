0a19e18d4cc924f35b58525ee74b013a5ff80a6d
krzysiekbienias <krzysiek.bienias.kr@gmail.com>
2025-11-14 20:26:56 +0100

    (feat): Add an iterative version of BFS traversal using queue together supported by display function.

--- header/binary_tree/bfs.hpp
+++ header/binary_tree/bfs.hpp
@@ -1,4 +1,6 @@
 #pragma once
 #include "binary_tree/binary_tree_node.hpp"
+#include<vector>
 
-void bfsPrint(BinaryTreeNode* tree);
+void bfsPrint(TreeNode* tree);
+std::vector<std::vector<int>> bsfTree(TreeNode* tree);
--- src/binary_tree/bfs.cpp
+++ src/binary_tree/bfs.cpp
@@ -1,19 +1,59 @@
 #include "binary_tree/binary_tree_node.hpp"
 #include "binary_tree/bfs.hpp"
-
+#include "std_aliases.hpp"
 #include <iostream>
 
-void bfsPrint(BinaryTreeNode* tree) {
+using namespace stl;
+
+vector<vector<int>> bsfTree(TreeNode* tree) {
+    vector<vector<int>> result;
+    if (tree==nullptr) return result;
+    queue<TreeNode*> q;
+    q.push(tree);
+
+    while (!q.empty()) {
+        int levelSize=q.size();
+        vector<int> currentLevel;
+        for (int i=0;i<levelSize;++i){
+            TreeNode* node=q.front();
+            q.pop();
+            currentLevel.push_back(node->m_value);
+            if (node->left) q.push(node->left);
+            if (node->right) q.push(node->right);
+        }
+        result.emplace_back(currentLevel);
+    }
+    return result;
+
+}
+
+// Helper function to print the result for bfsTree function
+void printResult(const std::vector<std::vector<int>>& result) {
+    std::cout << "[";
+    for (size_t i = 0; i < result.size(); i++) {
+        std::cout << "[";
+        for (size_t j = 0; j < result[i].size(); j++) {
+            std::cout << result[i][j];
+            if (j < result[i].size() - 1) std::cout << ",";
+        }
+        std::cout << "]";
+        if (i < result.size() - 1) std::cout << ",";
+    }
+    std::cout << "]" << std::endl;
+}
+
+
+void bfsPrint(TreeNode* tree) {
     if (!tree) {
         std::cout << "Tree is empty";
         return;
     }
-    std::queue<BinaryTreeNode *> q;
+    std::queue<TreeNode *> q;
     q.push(tree);
     while (!q.empty()) {
         int levelSize{static_cast<int>(q.size())};
         while (levelSize--) {
-            BinaryTreeNode *tempTree = q.front();
+            TreeNode *tempTree = q.front();
             q.pop();
             std::cout << tempTree->m_value << " ";
             if (tempTree->left) q.push(tempTree->left);
