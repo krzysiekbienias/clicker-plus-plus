221cc5194e9258865667a9180ac0aee072842bb0
krzysiekbienias <krzysiek.bienias.kr@gmail.com>
2025-12-02 02:11:44 +0100

    (refactor): refactor: disable copy and assignment for TreeNode to prevent double delete. Add addLeftChild and addRightChild to support building a binary tree when we have use a parent pointer.

--- header/binary_tree/binary_tree_node.hpp
+++ header/binary_tree/binary_tree_node.hpp
@@ -1,23 +1,42 @@
 #pragma once
 
 class TreeNode {
-    public:
-        int m_value;
-        TreeNode* left;
-        TreeNode* right;
-        TreeNode* parent;
-        //constructor
+public:
+    int m_value;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode *parent;
+    //constructor
 
-        explicit TreeNode(int value) {
-            this->m_value = value;
-            left = nullptr;
-            right = nullptr;
-            parent= nullptr;
-        }
+    explicit TreeNode(int value) {
+        this->m_value = value;
+        left = nullptr;
+        right = nullptr;
+        parent = nullptr;
+    }
 
-        //destructor
-        ~TreeNode() {
-            delete left;
-            delete right;
-        }
+    //destructor
+    ~TreeNode() {
+        delete left;
+        delete right;
+    }
+    //block default copy
+    TreeNode(const TreeNode &) = delete;
+    //block default assignment
+    TreeNode &operator=(const TreeNode &) = delete;
+
+    //use when you want to create BinaryTree with parent pointer
+    TreeNode* addLeftChild(int value) {
+        auto *child =new TreeNode(value);
+        child->parent =this;
+        this->left=child;
+        return child;
+    }
+
+    TreeNode* addRightChild(int value) {
+        auto *child =new TreeNode(value);
+        child->parent =this;
+        this->right=child;
+        return child;
+    }
 };
