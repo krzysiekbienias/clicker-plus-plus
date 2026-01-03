51da666a033d38e13d22ec5be23c757358d21749
krzysiekbienias <krzysiek.bienias.kr@gmail.com>
2026-01-03 12:42:25 +0100

    feat(graph): overload operator<< for Graph::Edge

--- header/graphs/graph_interview_mode.hpp
+++ header/graphs/graph_interview_mode.hpp
@@ -2,15 +2,20 @@
 #include "std_aliases.hpp"
 
 using namespace stl;
+using namespace str;
 
 using Vertex=int;
 
 struct Edge {
     Vertex to;
     int w;
+
+    friend ostream &operator<<(ostream &os, const Edge &e) {
+        return os << "--(" << e.w << ")--" << e.to;
+    }
 };
 
 
 using GraphLC = vector<vector<Edge>>;
 
-GraphLC buildGraph(int n,vector<vector<int>> vecRep);
+GraphLC buildGraph(int n,const vector<vector<int>>& vecRep);
