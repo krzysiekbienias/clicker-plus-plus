0d0a91dcb945554b18f4d2c0c657a5195a66aac5
krzysiekbienias <krzysiek.bienias.kr@gmail.com>
2026-01-07 14:31:19 +0100

    robust(first missing positive): Add a more optimal solution for the problem with cyclic placement approach.

--- header/arrays/first_missing_positive.hpp
+++ header/arrays/first_missing_positive.hpp
@@ -1,3 +1,6 @@
 #pragma once
 
-int firstMissingPositive(vector<int>& nums);
+
+int firstMissingPositiveSortingAllowed(std::vector<int>& nums);
+
+int firstMissingPositive(std::vector<int>& nums);
--- src/arrays/first_missing_positive.cpp
+++ src/arrays/first_missing_positive.cpp
@@ -2,20 +2,49 @@
 using namespace stl;
 
 //sorting allowed
-int firstMissingPositive(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int need=1;
-        //disregard all negative
-        auto it=std::lower_bound(nums.begin(),nums.end(),1);
-        for (it ;it!=nums.end();++it) {
-            int x=*it;
-            if (x<need){
-                continue;
-            }
-            if (need==x) {
-                need++; //
-            }
-            else return need;
+int firstMissingPositiveSortingAllowed(vector<int>& nums)
+{
+    sort(nums.begin(), nums.end());
+    int candidate = 1;
+    //disregard all negative
+    auto it = std::lower_bound(nums.begin(), nums.end(), 1);
+    for (it; it != nums.end(); ++it)
+    {
+        int x = *it;
+        if (x < candidate)
+        {
+            continue;
         }
-        return need;
+        if (candidate == x)
+        {
+            candidate++; //
+        }
+        else return candidate;
     }
+    return candidate;
+}
+
+
+int firstMissingPositive(vector<int>& nums)
+{
+    if (nums.empty()) return 1;
+    int n = nums.size();
+    for (int i = 0; i < n; ++i)
+    {
+        int x = nums[i];
+        while (x <= n && x >= 1)
+        {
+            int targetIndex = x - 1;
+            //if x is in the right place, or we have duplicate ->break
+            if (nums[targetIndex]==x) break;
+            std::swap(nums[i],nums[targetIndex]);
+            //update x
+            x=nums[i];
+        }
+    }
+    for (int i = 0; i < n; ++i)
+    {
+        if (nums[i] != i + 1) return i + 1;
+    }
+    return n + 1;
+}
