64459a7e39d3d4251351a8c967a1a7681c6d4bfa
krzysiekbienias <krzysiek.bienias.kr@gmail.com>
2026-01-13 23:27:32 +0100

    fix (dynamic-programming): remove the redundant condition in ways to climb problem.

--- src/dynamic_programming/ways_to_climb.cpp
+++ src/dynamic_programming/ways_to_climb.cpp
@@ -3,14 +3,17 @@
 
 
 // Implement your ways_to_climb logic here.
-int waysToClimb(int n,const std::vector<int>& jumps){
-    std::vector<int> dp(n+1,0);
-    dp[0]=1; // There’s 1 way to reach step 0: do nothing.
-    for (size_t i=1;i<n+1;++i){
-        for (int jump:jumps){
-            if(i-jump<0)continue; //save guard becasue without it failed one test: EXPECT_EQ(waysToClimb(1, {1, 2}), 1)
-            if(i>=jump){
-                dp[i]+=dp[i-jump];
+int waysToClimb(int n, const std::vector<int>& jumps)
+{
+    std::vector<int> dp(n + 1, 0);
+    dp[0] = 1; // There’s 1 way to reach step 0: do nothing.
+    for (size_t i = 1; i < n + 1; ++i)
+    {
+        for (int jump : jumps)
+        {
+            if (i -jump>= 0)
+            {
+                dp[i] += dp[i - jump];
             }
         }
     }
