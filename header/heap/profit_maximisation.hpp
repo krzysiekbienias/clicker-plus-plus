52420b586c85884dff97d2c846879c7e8659674c
krzysiekbienias <krzysiek.bienias.kr@gmail.com>
2026-01-16 23:27:00 +0100

    feat: (heap) Add profit maximization from the ticket problem using heap.

---
+++ header/heap/profit_maximisation.hpp
@@ -0,0 +1,5 @@
+#pragma once
+#include <vector>
+// Declare your interface here.
+
+int profitMaximisation(const std::vector<int> &tickets, int k) ;
---
+++ src/heap/profit_maximisation.cpp
@@ -0,0 +1,22 @@
+#include<std_aliases.hpp>
+
+using namespace stl;
+int profitMaximisation(const vector<int> &tickets, int k) {
+    if (tickets.empty()||k==0) return 0;
+    priority_queue<int> pq; //maxHeap
+    for (int ticket:tickets) {
+        pq.push(ticket);
+    }
+    int maxProfit=0;
+    int i=0;
+    while (i<k) {
+        int top=pq.top();
+        maxProfit+=top;
+        top--;
+        pq.pop();
+        pq.push(top);
+        i++;
+    }
+    return maxProfit;
+}
+
