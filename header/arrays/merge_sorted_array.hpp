a18d5067cbd9e51b58c29db109beef87a752e0e6
krzysiekbienias <krzysiek.bienias.kr@gmail.com>
2026-01-17 22:38:47 +0100

    feat(two pointers): provide scaffold for merge sorted array.

---
+++ header/arrays/merge_sorted_array.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+void merge(std::vector<int>& nums1, int m, std::vector<int>& nums2, int n);
---
+++ src/arrays/merge_sorted_array.cpp
@@ -0,0 +1,11 @@
+#include <std_aliases.hpp>
+
+using namespace stl;
+using namespace io;
+using namespace str;
+
+void merge(std::vector<int>& nums1, int m, std::vector<int>& nums2, int n)
+{
+
+}
+
