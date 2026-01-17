411a25653028638b06e0e1f11aa8b67818e9d461
krzysiekbienias <krzysiek.bienias.kr@gmail.com>
2026-01-17 17:04:58 +0100

    refactor(bit manipulation): remove intermediate bitset variables in date binary conversion

--- src/bit_manipulation/convert_date_to_binary.cpp
+++ src/bit_manipulation/convert_date_to_binary.cpp
@@ -6,29 +6,25 @@ using namespace str;
 
 void removeLeadingZeros(string& str)
 {
-    int i=0;
-    while (i<str.size() && str[i]=='0')
+    int i = 0;
+    while (i < str.size() && str[i] == '0')
     {
         i++;
     }
-    str=str.substr(i);
+    str = str.substr(i);
 }
 
 // Implement your logic here.
 string convertDateToBinary(string date)
-{
-    string year=date.substr(0,4);
-    string month=date.substr(5,2);
-    string day=date.substr(8,2);
-    std::bitset<16> yearBit(stoi(year));
-    std::bitset<16> monthBit(stoi(month));
-    std::bitset<16> dayBit(stoi(day));
-    string yearBinary=yearBit.to_string();
-    string monthBinary=monthBit.to_string();
-    string dayBinary=dayBit.to_string();
+{   if (date.empty()) return "0";
+    string year = date.substr(0, 4);
+    string month = date.substr(5, 2);
+    string day = date.substr(8, 2);
+    string yearBinary=std::bitset<16> (stoi(year)).to_string();
+    string monthBinary=std::bitset<16> (stoi(month)).to_string();
+    string dayBinary=std::bitset<16> (stoi(day)).to_string();
     removeLeadingZeros(yearBinary);
     removeLeadingZeros(monthBinary);
     removeLeadingZeros(dayBinary);
-    return yearBinary+"-"+monthBinary+"-"+dayBinary;
-
+    return yearBinary + "-" + monthBinary + "-" + dayBinary;
 }
