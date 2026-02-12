daef08b443f382f24bc819d59242bca2ffb51ee2
krzysiekbienias <krzysiek.bienias.kr@gmail.com>
2026-02-12 23:34:13 +0100

    refactor: add to heder bubble sort version without debug mode

--- header/sorting/bubble_sort.hpp
+++ header/sorting/bubble_sort.hpp
@@ -2,5 +2,7 @@
 #include <vector>
 // Declare your bubble_sort interface here.
 
-std::vector<int> bubbleSort(std::vector<int>& array);
-   
+std::vector<int> bubbleSortDebug(std::vector<int>& array);
+
+std::vector<int> bubbleSort(std::vector<int> nums);
+
