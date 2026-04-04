4cd1167b3e8fd9685fd88c36d4aafd9f21431adf
krzysiekbienias <krzysiek.bienias.kr@gmail.com>
2026-04-04 11:53:33 +0200

    perf: optimize anagram grouping by reducing redundant lookups and copies

--- header/strings/group_anagrams.hpp
+++ header/strings/group_anagrams.hpp
@@ -4,4 +4,4 @@
 
 // Declare your group_anagrams interface here.
 
-std::vector<std::vector<std::string>> groupAnagrams(std::vector<std::string>words);
+std::vector<std::vector<std::string>> groupAnagrams(const std::vector<std::string>& words);
