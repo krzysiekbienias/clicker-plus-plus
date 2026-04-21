ca3e32c92679bd72d75a6c836b41664360b1a404
krzysiekbienias <krzysiek.bienias.kr@gmail.com>
2026-04-21 20:52:57 +0200

    refactor(strings): improve `QuestionsMarks` function readability with consistent formatting and spacing

--- src/pattern_matching/question_marks.cpp
+++ src/pattern_matching/question_marks.cpp
@@ -2,27 +2,29 @@
 
 using namespace stl;
 
-string QuestionsMarks(const string &str) {
-    int lastDigit=-1;
-    int q=0;
-    bool foundPair=false;
-    int n=str.size();
-    for (int i=0;i<n;++i) {
-        char c=str[i];
-        if (c>='0' && c<='9') {
-            int digit=c-'0';
-            if (lastDigit!=-1 && lastDigit+digit==10) {
-                if (q!=3) return "false";
-                else foundPair=true;
+string QuestionsMarks(const string& str) {
+    int lastDigit = -1;
+    int q = 0;
+    bool foundPair = false;
+    int n = str.size();
+    for (int i = 0; i < n; ++i) {
+        char c = str[i];
+        if (c >= '0' && c <= '9') {
+            int digit = c - '0';
+            if (lastDigit != -1 && lastDigit + digit == 10) {
+                if (q != 3)
+                    return "false";
+                else
+                    foundPair = true;
             }
-            lastDigit=digit;
-            q=0;
-        }
-        else if (c=='?' && lastDigit!=-1) {
+            lastDigit = digit;
+            q = 0;
+        } else if (c == '?' && lastDigit != -1) {
             q++;
         }
     }
-    if (foundPair==false) return "false";
+    if (foundPair == false)
+        return "false";
 
     return "true";
 }
