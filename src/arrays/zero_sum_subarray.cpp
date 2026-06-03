8cbdccdd3322c3253b1eb8c4a33607ae1dad6666
krzysiekbienias <krzysiek.bienias.kr@gmail.com>
2026-06-03 12:45:55 +0200

    fix(arrays): improve formatting and comments in zeroSumSubarray function
    - Reformatted the zeroSumSubarray function for better readability.
    - Updated comments for clarity regarding prefix sum checks.

--- src/arrays/zero_sum_subarray.cpp
+++ src/arrays/zero_sum_subarray.cpp
@@ -5,15 +5,14 @@ using namespace io;
 using namespace str;
 
 // Implement your logic here.
-bool zeroSumSubarray(const vector<int>& nums)
-{
-    if (nums.empty()) return false;
+bool zeroSumSubarray(const vector<int>& nums) {
+    if (nums.empty())
+        return false;
     int prefixSum = 0;
     unordered_set<int> seenPrefixSum;
-    for (int x : nums)
-    {
+    for (int x : nums) {
         prefixSum += x;
-        //we want to return true if the prefix has been already seen.
+        // we want to return true if the prefix has been already seen.
         bool inserted = seenPrefixSum.insert(prefixSum).second;
         if (prefixSum == 0 || !inserted)
             return true;
